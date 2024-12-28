07d729d43c34356cb1d38b4088ad890311a42889
aisyah551 <nooraisyah551@gmail.com>
2024-12-29 07:16:29 +0800

    Parcelable

--- app/src/main/java/com/example/streamliner/Question.java
+++ app/src/main/java/com/example/streamliner/Question.java
@@ -1,30 +1,73 @@
 package com.example.streamliner;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class Question {
+public class Question implements Parcelable {
     private String title;
     private List<String> answers;
-    private int correctAnswerIndex;
+    private int correctIndex;
 
     public Question() {}
 
     public Question(String title, List<String> answers, int correctAnswerIndex) {
         this.title = title;
         this.answers = answers;
-        this.correctAnswerIndex = correctAnswerIndex;
+        this.correctIndex = correctAnswerIndex;
     }
 
     // Getters and Setters
     public String getTitle() { return title; }
 
-    public void setTitle(String id) { this.title = title; }
+    public void setTitle(String title) { this.title = title; }
 
     public List<String> getAnswers() { return answers; }
 
     public void setAnswers(List<String> answers) { this.answers = answers; }
 
-    public int getCorrectAnswerIndex() { return correctAnswerIndex; }
+    public int getCorrectIndex() { return correctIndex; }
 
-    public void setCorrectAnswerIndex(int correctAnswerIndex) { this.correctAnswerIndex = correctAnswerIndex; }
+    public void setCorrectIndex(int correctAnswerIndex) { this.correctIndex = correctAnswerIndex; }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "title='" + title + '\'' +
+                ", answers=" + answers +
+                ", correctAnswerIndex=" + correctIndex +
+                '}';
+    }
+
+    // Parcelable implementation
+    protected Question(Parcel in) {
+        title = in.readString();
+        answers = in.createStringArrayList();
+        correctIndex = in.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeStringList(answers);
+        dest.writeInt(correctIndex);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Question> CREATOR = new Creator<Question>() {
+        @Override
+        public Question createFromParcel(Parcel in) {
+            return new Question(in);
+        }
+
+        @Override
+        public Question[] newArray(int size) {
+            return new Question[size];
+        }
+    };
 }
