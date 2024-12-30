31d6ba4cbdb0a131443b5df2a4d2dae5688ac358
ThamJingQin <thamjingqin@gmail.com>
2024-12-30 18:42:28 +0800

    Added splash screens, change activities to fragments

---
+++ app/src/main/java/com/example/streamliner/RegisterPage.java
@@ -0,0 +1,153 @@
+package com.example.streamliner;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterPage extends Fragment {
+    private EditText ETName, ETPhone, ETEmail, ETPassword, ETConfirmPassword;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_register_page, container, false);
+
+        // Initialize UI elements
+        ETName = view.findViewById(R.id.ETName);
+        ETPhone = view.findViewById(R.id.ETPhone);
+        ETEmail = view.findViewById(R.id.ETEmail);
+        ETPassword = view.findViewById(R.id.ETPwd);
+        ETConfirmPassword = view.findViewById(R.id.ETConfirmPwd);
+
+        TextView TVGoToSignIn = view.findViewById(R.id.TVGoToSignIn);
+        Button btnSignUp = view.findViewById(R.id.BTSignUp);
+
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        // Sign Up button click listener
+        btnSignUp.setOnClickListener(v -> {
+            String textName = ETName.getText().toString();
+            String textPhone = ETPhone.getText().toString();
+            String textEmail = ETEmail.getText().toString();
+            String textPassword = ETPassword.getText().toString();
+            String textConfirmPassword = ETConfirmPassword.getText().toString();
+
+            if (TextUtils.isEmpty(textName)) {
+                showError(ETName, "Name is required");
+            } else if (TextUtils.isEmpty(textPhone)) {
+                showError(ETPhone, "Phone number is required");
+            } else if (textPhone.length() != 10 && textPhone.length() != 11) {
+                showError(ETPhone, "Phone number should be 10 or 11 digits");
+            } else if (TextUtils.isEmpty(textEmail)) {
+                showError(ETEmail, "Email is required");
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()) {
+                showError(ETEmail, "Valid email is required");
+            } else if (TextUtils.isEmpty(textPassword)) {
+                showError(ETPassword, "Password is required");
+            } else if (TextUtils.isEmpty(textConfirmPassword)) {
+                showError(ETConfirmPassword, "Password confirmation is required");
+            } else if (!textPassword.equals(textConfirmPassword)) {
+                showError(ETConfirmPassword, "Passwords do not match");
+            } else {
+                registerUser(textName, textPhone, textEmail, textPassword);
+            }
+        });
+
+        // Navigate to LoginFragment
+        TVGoToSignIn.setOnClickListener(v -> {
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.nav_host_fragment_activity_main, new LoginPage())
+                    .addToBackStack(null)
+                    .commit();
+        });
+
+        return view;
+    }
+
+    private void showError(EditText editText, String error) {
+        Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+        editText.setError(error);
+        editText.requestFocus();
+    }
+
+    private void registerUser(String textName, String textPhone, String textEmail, String textPassword) {
+        auth.createUserWithEmailAndPassword(textEmail, textPassword).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(getContext(), "User registered successfully", Toast.LENGTH_LONG).show();
+
+                FirebaseUser firebaseUser = auth.getCurrentUser();
+
+                // Update display name
+                UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(textName).build();
+                firebaseUser.updateProfile(profileChangeRequest);
+
+                // Save user data into Firestore
+                Map<String, Object> userDetails = new HashMap<>();
+                userDetails.put("name", textName);
+                userDetails.put("phone", textPhone);
+                userDetails.put("email", textEmail);
+
+                firestore.collection("users").document(firebaseUser.getUid())
+                        .set(userDetails)
+                        .addOnCompleteListener(userTask -> {
+                            if (userTask.isSuccessful()) {
+                                Toast.makeText(getContext(), "User registered successfully.", Toast.LENGTH_LONG).show();
+                                // Navigate to LoginFragment
+                                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                                fragmentManager.beginTransaction()
+                                        .replace(R.id.nav_host_fragment_activity_main, new LoginPage())
+                                        .commit();
+                            } else {
+                                Toast.makeText(getContext(), "User registration failed. Try again.", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            } else {
+                try {
+                    throw task.getException();
+                } catch (FirebaseAuthInvalidCredentialsException e) {
+                    showError(ETEmail, "Your email is invalid or already in use.");
+                } catch (FirebaseAuthUserCollisionException e) {
+                    showError(ETEmail, "User is already registered with this email.");
+                } catch (Exception e) {
+                    Log.e("RegisterError", e.getMessage());
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/streamliner/VerifyEmailPage.java
@@ -0,0 +1,75 @@
+package com.example.streamliner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class VerifyEmailPage extends Fragment {
+
+
+    private String email;
+    private String correctCode;
+
+    private EditText ETCode;
+    private Button BTContinue;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_verify_email_page, container, false);
+
+        ETCode = view.findViewById(R.id.ETCode);
+        BTContinue = view.findViewById(R.id.BTContVerifyEmail);
+
+        // Retrieve arguments passed from parent Activity or Fragment
+        if (getArguments() != null) {
+            email = getArguments().getString("email");
+            correctCode = getArguments().getString("verificationCode");
+        }
+
+        BTContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredCode = ETCode.getText().toString();
+
+                // Verify the code
+                if (TextUtils.isEmpty(enteredCode)) {
+                    ETCode.setError("Code is required");
+                    ETCode.requestFocus();
+                    return;
+                } else if (enteredCode.equals(correctCode)) {
+                    // Code is correct, navigate to Reset Password Fragment or Activity
+                    ResetPassword resetPasswordFragment = new ResetPassword();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("email", email);
+                    resetPasswordFragment.setArguments(bundle);
+
+                    // Use FragmentManager to replace the current Fragment
+                    requireActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.nav_host_fragment_activity_main, resetPasswordFragment)
+                            .addToBackStack(null)
+                            .commit();
+                } else {
+                    Toast.makeText(requireContext(), "Invalid code", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
+        return view;
+    }
+}
