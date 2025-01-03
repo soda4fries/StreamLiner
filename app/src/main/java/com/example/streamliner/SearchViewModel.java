affd6e22c29ce5b548343f2a282ad52c14ac4a91
soda4fries <39405916+soda4fries@users.noreply.github.com>
2025-01-03 23:42:22 +0800

    merging

--- app/src/main/java/com/example/streamliner/SearchViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.streamliner;
-
-import androidx.lifecycle.ViewModel;
-
-public class SearchViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/streamliner/TopCoursesViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.streamliner;
-
-import androidx.lifecycle.ViewModel;
-
-public class TopCoursesViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
